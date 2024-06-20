b0757b57579d4ed7c7c16c4fb67aa605b75742b7
TheCorruptedBit <46328503+TheCorruptedBit@users.noreply.github.com>
2024-06-20 15:02:32 -0400

    pc mark 1

---
+++ source/pc.sv
@@ -0,0 +1,36 @@
+module pc(
+    output [31:0] pc_out,
+    output [31:0] pc_add_4,
+    input [31:0] generated_immediate,
+    input logic branch_decision,
+    input [31:0] pc_write_value,
+    input logic pc_immediate_jump,
+    logic clock,
+    logic reset
+);
+
+reg [31:0] current_pc;
+logic [31:0] next_pc;
+
+always_comb begin
+    logic [31:0] pc_4;
+    logic [31:0] pc_add_immediate;
+
+    assign pc_4 = current_pc + 4;
+    assign pc_add_4 = pc_4;
+    assign pc_add_immediate = pc_immediate_jump ? pc_write_value : current_pc + generated_immediate;
+
+    assign next_pc = branch_decision ? pc_add_immediate : pc_4;
+end
+
+always_ff @(posedge clock, negedge reset) begin
+    if(~reset) begin
+        current_pc = 0;
+    end
+    else begin
+        current_pc = next_pc;
+    end
+
+end
+
+endmodule
--- testbench/tb.sv
+++ testbench/tb.sv
@@ -1,6 +1,11 @@
 `timescale 1ms/10ps
 module tb;
 
-    
+
+initial begin
+    // make sure to dump the signals so we can see them in the waveform
+    $dumpfile("sim.vcd");
+    $dumpvars(0, tb);
+
 
 endmodule
