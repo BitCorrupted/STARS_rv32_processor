3a3121738cca00a2f32546226aaa1c6fc6703a7c
mlisondo <mateo.esteban.pr@gmail.com>
2024-06-22 17:20:29 -0400

    ALU tb

--- testbench/ALU_tb.sv
+++ testbench/ALU_tb.sv
@@ -203,8 +203,8 @@ module tb;
             #1;
             check_result();
             #1;
-            // check_flag();        //is there a way to check flags on random values?
-            // #1;
+            check_flag();
+            #1;
 
         end
     endtask
@@ -299,14 +299,80 @@ module tb;
             #1;
             check_result();
             #1;
-            check_flag();        //is there a way to check flags on random values?
+            check_flag();
             #1;
 
         end
     endtask
 
-    task logical_shift;
+    task SHIFT_LEFT_LOGICAL;
+        
+        // Basic functional test
+        tb_rda = 32'h0000_0001;
+        tb_rdb = 1;
+        tb_fop = FOP_SLL;
+        exp_result = tb_rda << tb_rdb;
+        exp_Z = (exp_result == 0) ? 1 : 0;
+        exp_N = exp_result[31];
+        #1;
+        check_result();
+        #1;
+        check_flag();
+        #1;
 
+        // Shift left by 0
+        tb_rda = 32'h8000_0000; // Large negative number
+        tb_rdb = 0;
+        tb_fop = FOP_SLL;
+        exp_result = tb_rda << tb_rdb;
+        exp_Z = (exp_result == 0) ? 1 : 0;
+        exp_N = exp_result[31];
+        #1;
+        check_result();
+        #1;
+        check_flag();
+        #1;
+
+        // Edge case: Shift left by 31
+        tb_rda = 32'h0000_0001; // 1
+        tb_rdb = 31;            // Shift left by 31
+        tb_fop = FOP_SLL;
+        exp_result = tb_rda << tb_rdb;
+        exp_Z = (exp_result == 0) ? 1 : 0;
+        exp_N = exp_result[31];
+        #1;
+        check_result();
+        #1;
+        check_flag();
+        #1;
+
+        // Shift resulting in zero
+        tb_rda = 32'h0000_0001; // 1
+        tb_rdb = 32;            // Shift left by 32
+        tb_fop = FOP_SLL;
+        exp_result = 0;
+        exp_Z = (exp_result == 0) ? 1 : 0;
+        exp_N = exp_result[31];
+        #1;
+        check_result();
+        #1;
+        check_flag();
+        #1;
+
+        // Random tests
+        repeat (10) begin
+            tb_rda = $random;
+            tb_rdb = $random % 32; // Limiting shift amount to 31
+            tb_fop = FOP_SLL;
+            exp_result = tb_rda << tb_rdb;
+            exp_Z = (exp_result == 0) ? 1 : 0;
+            exp_N = exp_result[31];
+            #1;
+            check_result();
+            #1;
+            check_flag();
+            #1;
+        end
     endtask
 
     initial begin
@@ -318,7 +384,13 @@ module tb;
         //0-33 ms
         SUB;
         #1;
-        //33-66 ms
+        //33-86 ms
+        SHIFT_LEFT_LOGICAL;
+        #1;
+        //86-128 ms
+        SHIFT_RIGHT_LOGICAL;
+        #1;
+        //128-
 
         $finish;
     end
