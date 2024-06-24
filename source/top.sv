b2d53de3ca49cf2deb19335e5f24eb0166d37ddd
mlisondo <mateo.esteban.pr@gmail.com>
2024-06-24 15:35:35 -0400

    top

--- source/top.sv
+++ source/top.sv
@@ -32,21 +32,19 @@ module top (
 
     
    
-    logic [31:0] register_write_data,
-    logic [31:0] regA_data, regB_data,
+    logic [31:0] register_write_data;
+    logic [31:0] regA_data, regB_data;
 
-    logic [31:0] program_counter,
-    logic [31:0] program_counter_out,
-    logic branch_choice
+    logic [31:0] program_counter;
+    logic [31:0] program_counter_out;
+    logic branch_choice;
   
 
-    logic [31:0] result,
-    logic Z, N, C, V,
+    logic [31:0] result;
+    logic Z, N, C, V;
 
-    logic [31:0] imm_gen,
-    logic alu_mux_en,
-  logic [31:0] rdb,
-    logic b_out
+    logic [31:0] rdb;
+    logic b_out;
   
   //this is a test
   ram ram(.clk(hz100), .rst(reset), .data_address(), .instruction_address(), .dm_read_en(), .dm_write_en(), .data_to_write(), .instruction_read(), .data_read());
@@ -56,25 +54,25 @@ module top (
   control_logic_unit control_logic(.i_type(i_type), .instruction(instruction), .alu_op(alu_op), .branch_type(branch_type), .reg_write_en(reg_write_en), .alu_mux_en(alu_mux_en), .store_byte(store_byte),
   .mem_to_reg(mem_to_reg), .pc_absolute_jump_vec(pc_absolute_jump_vec), .load_byte(load_byte), .read_next_pc(read_next_pc), .write_mem(write_mem), .read_mem(read_mem));
 
-  imm_generator imm_generator(.inst(inst), .type_i(i_type), .imm_gen(imm_gen));
-
-  alu_mux alu_mux(.imm_gen(imm_gen), .reg_b(regB_data), .alu_mux(alu_mux_en), .rdb(rdb));
-
   branch_logic branch_logic(.branch_type(branch_type), .ALU_neg_flag(N), .ALU_overflow_flag(V), .ALU_zero_flag(Z), .b_out(branch_choice));
 
   pc pc(.pc_out(program_counter), .pc_add_4(program_counter_out), .generated_immediate(imm_gen), .branch_decision(branch_choice), .pc_write_value(regA_data), .pc_immediate_jump(pc_absolute_jump_vec), .in_en(), .auipc_in(alu_mux_en), .clock(hz100), .reset(reset));
-
-  ALU ALU(.rda(regA), .rdb(regB), .fop(alu_op), .result(result), .Z(Z), .N(N), .C(C), .V(V));
 
   register_file register_file(.clk(hz100), .rst(reset), .regA_address(regA), .regB_address(regB), .rd_address(rd), .register_write_en(reg_write_en), .register_write_data(register_write_data), .regA_data(regA_data), .regB_data(regB_data));
 
   writeback writeback(.memory_value(), .ALU_value(result), .pc_4_value(program_counter_out), .mem_to_reg(mem_to_reg), .load_byte(load_byte), .read_pc_4(read_next_pc), .register_write(register_write_data));
 
-  byte_demux byte_demux(.reg_b(), .store_byte_en(), .reg_b_out(), .b_out());
- 
 
 
+  byte_demux byte_demux(.reg_b(regB_data), .store_byte_en(store_byte), .reg_b_out(data_to_write), .b_out(b_out));
 
+  byte_imm_gen byte_immediate_generator (.b_out(b_out), .imm_gen_byte(data_to_write));
+
+  ALU ALU(.rda(regA_data), .rdb(regB_data), .fop(alu_op), .result(result), .Z(Z), .N(N), .C(C), .V(V));
+
+  imm_generator imm_generator(.inst(inst), .type_i(i_type), .imm_gen(imm_gen));
+
+  alu_mux alu_mux(.imm_gen(imm_gen), .reg_b(regB_data), .alu_mux_en(alu_mux_en), .rdb(rdb));
 
 
 
