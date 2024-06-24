52f78b63590d151a5981845bdf40f7cb5cb2ea11
TheCorruptedBit <46328503+TheCorruptedBit@users.noreply.github.com>
2024-06-24 12:24:18 -0400

    auipc support added

--- testbench/pc_testbench.sv
+++ testbench/pc_testbench.sv
@@ -7,10 +7,11 @@ logic test_branch_decision = 0;
 logic [31:0] test_pc_write_value = 0;
 logic test_pc_immediate_jump = 0;
 logic test_in_en = 0;
+logic test_auipc_in = 0;
 logic [31:0] test_pc;
 logic [31:0] last_pc;
 logic [31:0] test_pc_4;
-pc testpc(test_pc, test_pc_4, test_gen_i, test_branch_decision, test_pc_write_value, test_pc_immediate_jump, test_in_en, clock, reset);
+pc testpc(test_pc, test_pc_4, test_gen_i, test_branch_decision, test_pc_write_value, test_pc_immediate_jump, test_in_en, test_auipc_in, clock, reset);
 integer total_tests = 0;
 integer passed_tests = 0;
 
@@ -70,22 +71,27 @@ initial begin
         last_pc = test_pc;
         test_branch_decision = $random > 80000000 ? 1 : 0;
         test_pc_immediate_jump = $random > 80000000 ? 1 : 0;
+        test_auipc_in = test_pc_immediate_jump || test_branch_decision ? 0 : $random > 80000000 ? 1 : 0;
         test_in_en = $random > 80000000 ? 1 : 0;
 
         test_gen_i = $random;
         test_gen_i = test_gen_i / 4;
         test_pc_write_value = $random;
+        #1;
+        if(test_auipc_in)
+            test_value(test_pc_4, last_pc + test_gen_i, "auipc error");
+        else
+            test_value(test_pc_4, last_pc + 4, "PC_4 Add 4 Error");
 
-        test_value(test_pc_4, last_pc + 4, "PC_4 Add 4 Error");
         pulse_clock;
         if(~test_in_en)
             test_value(test_pc, last_pc, "PC Write Disable Error");
         else begin
             if(test_branch_decision) begin
                 if(test_pc_immediate_jump)
-                    test_value(test_pc, test_pc_write_value + test_gen_i * 2, "PC Abs. Jump Error");
+                    test_value(test_pc, test_pc_write_value + test_gen_i, "PC Abs. Jump Error");
                 else
-                    test_value(test_pc, last_pc + test_gen_i * 2, "PC Rel. Jump Error");
+                    test_value(test_pc, last_pc + test_gen_i, "PC Rel. Jump Error");
             end
             else
                 test_value(test_pc, last_pc + 4, "PC Non-Branch Error");
@@ -129,6 +135,7 @@ module pc(
     input [31:0] pc_write_value,
     input logic pc_immediate_jump,
     input logic in_en,
+    input logic auipc_in,
     input logic clock,
     input logic reset
 );
@@ -139,16 +146,16 @@ logic [31:0] pc_4;
 logic [31:0] pc_add_immediate;
 
 always_comb begin
+    pc_add_immediate = pc_immediate_jump ? pc_write_value + generated_immediate: current_pc + generated_immediate;
     pc_4 = current_pc + 4;
-    pc_add_immediate = pc_immediate_jump ? pc_write_value + {generated_immediate[30:0],1'b0}: current_pc + {generated_immediate[30:0],1'b0};
 
     next_pc = branch_decision ? pc_add_immediate : pc_4;
 end
-assign pc_add_4 = pc_4;
+assign pc_add_4 = auipc_in ? pc_add_immediate : pc_4;
 
 always_ff @(posedge clock, negedge reset) begin
     if(~reset) begin
-        current_pc = 0; //placeholder constant
+        current_pc = 0; //placeholder constant for initialization
     end
     else begin
         if(in_en)
