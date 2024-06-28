b4654413b58315cebd93c30cff285d3be1d76e14
TheCorruptedBit <46328503+TheCorruptedBit@users.noreply.github.com>
2024-06-28 13:13:58 -0400

    imm gen quick fix

--- source/top.sv
+++ source/top.sv
@@ -466,7 +466,7 @@ module imm_generator (
             3'd2 : imm_gen = {{20{inst[31]}}, inst[31:25], inst[11:7]}; //S
             3'd3 : imm_gen = {{20{inst[31]}}, inst[7], inst[30:25], inst [11:8], 1'b0}; //SB
             3'd5 : imm_gen = {inst[31:12], 12'd0}; //U
-            3'd4 : imm_gen = {{13{inst[31]}}, inst[19:12], inst[20], inst[30:21]}; //UJ
+            3'd4 : imm_gen = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21],1'b0}; //UJ
             default : imm_gen = '0;
         endcase
     end
