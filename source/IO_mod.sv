e836ef6564701c27225e37b81e7dad3663e1c2dc
Abhishek Kaushikkar <abhikaushikkar@gmail.com>
2024-07-06 19:27:13 -0700

    actual IO

--- source/IO_mod.sv
+++ source/IO_mod.sv
@@ -4,83 +4,49 @@ module IO_mod_robot(
     input logic [31:0] data_from_mem,
     input logic [31:0] data_address, data_to_write,
     output logic [31:0] data_read,
-    output logic [31:0] IO_out,
+    output logic [31:0] IO_out, 
     input logic [31:0] IO_in
 );
  logic [31:0] output_reg, input_reg, enable_reg;
  logic [31:0] next_output_reg, next_input_reg, next_enable_reg;
 
 
- always_comb begin
-
-    
-
-    next_output_reg = output_reg;
-    next_input_reg = IO_in;
-    next_enable_reg = enable_reg;
-
-    if (write_mem) begin
-        if (data_address == 32'hFFFFFFFF) begin
-            next_output_reg = data_to_write; 
-            data_read = data_from_mem;
-
-        end
-
-        else if(data_address == 32'hFFFFFFFD) begin 
-            next_enable_reg = data_to_write;
-            data_read = data_from_mem;
-
-        end
-
-        else begin
-            next_output_reg = output_reg;
-            next_enable_reg = enable_reg;
-            data_read = data_from_mem;
-        end
-
-    end
-
-    else if(read_mem) begin
-        if (data_address == 32'hFFFFFFFC) begin
-            data_read = input_reg;
-
-        end
-        
-        else begin
-            data_read = data_from_mem;
-            next_input_reg = IO_in; 
-
-        end
-    end
-
-    else begin
-        next_output_reg = output_reg;
-        next_input_reg = IO_in;
-        next_enable_reg = enable_reg;
-        data_read = data_from_mem;
-
-
-    end
-
- end
-
- assign IO_out = output_reg;
+ 
 
 
 always_ff @(posedge clk, posedge rst) begin
     if (rst) begin
-        output_reg <= '0;
+        IO_out <= '0;
         input_reg <= '0;
-        pwm_reg <= '0;
+        enable_reg <= '0;
     end
 
     else begin
-        output_reg <= next_output_reg;
-        input_reg <= next_input_reg;
+        IO_out <= next_output_reg;
+        input_reg <= IO_in;
         enable_reg <= next_enable_reg;
 
     end
 
 end
+
+always_comb begin
+
+    next_output_reg = IO_out;
+    next_enable_reg = IO_out;
+    data_read = data_from_mem;
+    case(data_address)
+        32'hFFFFFFFF: begin
+           next_output_reg = (write_mem) ? data_to_write : IO_out;
+        end
+        32'hFFFFFFFD: begin
+           next_enable_reg = (write_mem) ? data_to_write : IO_out;
+        end
+        32'hFFFFFFFC: begin
+           data_read = (read_mem) ? input_reg : data_from_mem;
+        end
+    endcase
+end
+
 
 endmodule
